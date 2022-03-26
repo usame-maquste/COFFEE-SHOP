da411b1d81976fa3e759e739eb412838978064a0
Jay-Addow <102184553+Jay-Addow@users.noreply.github.com>
2022-03-26 17:38:35 +0300

    Update Hurbad Shop.py

--- Hurbad Shop.py
+++ Hurbad Shop.py
@@ -1,42 +1,25 @@
 #We Are F-Society !!
-print("Hello, Welcome to Boqol-Soon Resturent !!!")
-name = input("What is your name ?\n")
-print ("Hello "+ name +" , Thank you so much for coming in today.\n\n")
-menu = "Broast Chiken, Double Egg, Shuwaarma, Milk Shake"
-print(name + ", What would you like from our menu list ? here is what we are serving today..\n"+menu)
-order = input()
-print("Sounds good " + name + ", We'll have that " + order + " ready for you in a moment.")
-                    .......................................
-#Let's start a coffee shop together!! We're going to build a coffee shop using some new Python programming concepts!!
 
-#Let's build robot Barista!!
-
-#We Are F-Society !!
 print("Hello, Welcome to HURBAD Coffee Shop  !!!\n")
+
 name = input("What is your name ?\n")
+
 print ("Hello "+ name +" , Thank you  so much for coming in today.\n")
+
 menu = "Black Coffee, Espresso, Latte Cappucino, Macchiato"
+
 print(name + ", What would you like from our menu today ? Here is what we are serving. .\n"+menu)
+
 order = input()
+
 price = 5
+
 quantity = input("How many coffees would you like ?\n")
+
 total = price * int(quantity)
+
+
 print("Thank you , Your total is: $" + str(total))
+
+
 print("Okey-dokey " + name + ", We'll have your " + quantity + " " + order + " ready for you in a moment.")
-#name = "Osame Adam Abdulle"
-#age = 21
-#actual_age = 21.45
-#print (name)
-#print (age)
-#print (type(name))
-#print (type(age))
-#age = 21
-#actual_age = 21.45
-#print (name)
-#print (age)
-#print (type(name))
-#print (type(age))
-#print (type(actual_age))
-#print (55-5)
-#math = 3*4-5/9
-#print(math)
